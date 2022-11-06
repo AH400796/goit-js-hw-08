ea0e88d7fbcde40376f4bf0a17e83ef4232e7e05
AH400796 <A.Hokhman@gmail.com>
2022-11-06 12:25:07 +0200

    final?

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}
+
+let timeStamp = 0;
+try {
+  timeStamp = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+} catch (error) {}
+
+if (timeStamp === 0) {
+  player.setCurrentTime(timeStamp);
+}
+player.setCurrentTime(timeStamp.seconds);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  emailInput: document.querySelector('input[type="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
+  submBtn: document.querySelector('button[type="submit"]'),
+};
+
+let localStorageData;
+try {
+  localStorageData = JSON.parse(localStorage.getItem('feedback-form-state'));
+} catch (error) {
+  alert('Ooops! Something wrong with localStorage data');
+}
+
+refs.emailInput.value =
+  !localStorageData || !localStorageData.hasOwnProperty('email')
+    ? ''
+    : localStorageData.email;
+
+refs.textarea.value =
+  !localStorageData || !localStorageData.hasOwnProperty('message')
+    ? ''
+    : localStorageData.message;
+
+const feedbackContent = { ...localStorageData };
+refs.form.addEventListener('input', throttle(onTextInput, 500));
+refs.submBtn.addEventListener('click', onSubmit);
+
+function onTextInput(event) {
+  feedbackContent[event.target.getAttribute('name')] = event.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(feedbackContent));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  localStorage.clear();
+  console.log(feedbackContent);
+  refs.emailInput.value = '';
+  refs.textarea.value = '';
+}
